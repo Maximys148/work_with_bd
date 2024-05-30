3388d930d8fa97ae1b86284a7daa6f6e37755785
HroNouS <dredgods@gmail.com>
2024-05-30 19:30:56 +0300

    Каркас для дальнейшей работы по созданию универсального сервиса рабоы тс БД по DTO;

--- src/main/java/com/hronous/Main.java
+++ src/main/java/com/hronous/Main.java
@@ -1,7 +1,13 @@
 package com.hronous;
 
+import com.hronous.annotations.Table;
+import com.hronous.services.ClassFinder;
+
 public class Main {
     public static void main(String[] args) {
+        ClassFinder finder = new ClassFinder();
+       // finder.findClasses("com.hronous.services", Table.class);
 
+       // finder.getClasses("com.hronous").forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/hronous/services/ClassFinder.java
@@ -0,0 +1,50 @@
+package com.hronous.services;
+
+
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClassFinder {
+
+    private Map<String, Class<?>> classes = new HashMap<>();
+
+    public void findClasses(String packageName, Class<?> clazz){
+        InputStream resourceAsStream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replace('.', '/'));
+        if (resourceAsStream == null){
+            System.err.println("Resource not found " + packageName);
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceAsStream));
+        Set<? extends Class<?>> collect = bufferedReader
+                .lines()
+                .filter(it -> it.endsWith(".class"))
+                .map(it -> getClass(it, packageName))
+                .collect(Collectors.toSet());
+        collect.forEach(System.out::println);
+    }
+
+    private Class<?> getClass(String className, String packageName) {
+        try {
+            return Class.forName( packageName + "." +className.substring(0, className.lastIndexOf('.')) );
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class not found " + className);
+            return null;
+        }
+    }
+
+    public void getClasses(String packageName) {
+       Reflections reflections = new Reflections(packageName , new SubTypesScanner(false));
+        Set<Class<?>> subTypesOf = reflections.getSubTypesOf(Object.class);
+
+
+    }
+}
